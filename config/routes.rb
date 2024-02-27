1d7f4aac9222135afdf563fd35fa0627817eee1f
manishaparajuli <manisaparajuli66@gmail.com>
2024-02-27 14:41:47 +1100

    created review controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  resources :restaurants
+  get 'reviews/new'
+  resources :restaurants do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-  resources :restaurants do
-    resources :reviews, only: [:new]
-  end
+  resources :reviews, only: [:new, :create ]
+end
+get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
