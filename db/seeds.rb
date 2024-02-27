7f4a2ba81bfed243db5400d0bf95b4e6d618802b
manishaparajuli <manisaparajuli66@gmail.com>
2024-02-27 13:46:59 +1100

    resturant's model created

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# puts "Cleaning database..."
+# Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+dishoom = { name: 'Dishoom', address: 'melbourne', phone_number: '0022244', category: 'chinese' }
+pizza_east = { name: 'Pizza East', address: 'London E1 6PQ', phone_number: '666444', category: 'italian' }
+kfc = { name: 'KFC', address: 'Sydney', phone_number: '0000000', category: 'japanese' }
+ragusa = { name: 'Ragusa', address: 'Williamstown', phone_number: '8888', category: 'french' }
+cherry_beans = {name: 'Cherry Beans', address: "Glenroy", phone_number: '1111111', category: 'belgian'}
+
+[dishoom, pizza_east, kfc, ragusa, cherry_beans].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
